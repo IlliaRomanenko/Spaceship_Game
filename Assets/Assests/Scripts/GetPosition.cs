1dcf4f038d3e51f7b74f851d386433aa5fe2b570
Mariia <q4mariia@gmail.com>
2025-05-27 22:12:56 +0200

    Added small ship and movement within the sphere

---
+++ Assets/Assests/Scripts/GetPosition.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class GetPosition : MonoBehaviour
+{
+
+    private Collider collider;
+
+    public static event Action OnPositionGot;
+    private void Awake()
+    {
+        collider = GetComponent<Collider>();
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter(Collider other)
+    {
+        OnPositionGot?.Invoke();
+        
+    }
+      private void OnTriggerExit(Collider other)
+    {
+        transform.gameObject.SetActive(true);
+    }
+
+}
--- Assets/Assests/Scripts/TurnOnOffActivator.cs
+++ Assets/Assests/Scripts/TurnOnOffActivator.cs
@@ -10,17 +10,16 @@ public class TurnOnOffActivator : MonoBehaviour
    [SerializeField] private Transform _fingerTip;
    [SerializeField] private Transform _ship;
    [SerializeField] private Transform _hand;
-   [SerializeField] private GameObject _object;
-   [SerializeField] private Transform _rig;
+   //[SerializeField] private GameObject _object;
    [SerializeField] private float _rotationSensitivity = 10f;
-   [SerializeField] private float _scaleVector = 20f;
-   [SerializeField] private Transform _position;
+   [SerializeField] private float _scaleFactor = 20f;
+  //[SerializeField] private Transform _position;
 
    private float _maxDistance;
-   private Vector3 _nintialHandPosition;
+   private Vector3 _initialHandPosition;
    private Vector3 _initialShipPosition;
    private Quaternion _initialShipRotation;
-   private Quaternion _initialHandRotation;
+   private bool _isHandInVolume = false;
    private XRGrabInteractable _simpleInteractable;
    //private IXRInteractor _baseInteractor;
    private bool _isActivated = false;
@@ -34,15 +33,28 @@ public class TurnOnOffActivator : MonoBehaviour
       _initialShipRotation = _ship.rotation;
    }
 
-  
+    private void Start()
+    {
+      GetPosition.OnPositionGot += Activated;
+    }
 
-   void OnTriggerEnter(Collider other)
+   private void Update()
+   {
+      if (_isActivated)
+      { 
+         Navigate();
+      }
+   }
+    
+    void OnTriggerEnter(Collider other)
    {
       if (other.transform.IsChildOf(_fingerTip))
       {
          if (_activator != null)
          {
+            _isHandInVolume = true;
             _activator.SetActive(true);
+            //
          }
       }
    }
@@ -51,56 +63,41 @@ public class TurnOnOffActivator : MonoBehaviour
    {
       if (other.transform.IsChildOf(_fingerTip))
       {
-         _isActivated = false;
+         _isHandInVolume = false;
+        
          if (_activator != null)
          {
+            _isActivated = false;
             _activator.SetActive(false);
          }
-         _object.SetActive(true);
-         _activator.SetActive(false);
          _ship.rotation = _initialShipRotation;
          _ship.position = _initialShipPosition;
       }
    }
    
-   private void Update()
-   {
-      if (_isActivated)
-      {
-         Navigate();
-      }
-   }
 
    public void Activated()
    {
-         _isActivated = true;
-         _object.SetActive(false);
-         _nintialHandPosition = transform.position;
-         _initialHandRotation = _hand.rotation;
+      if (_isHandInVolume)
+      {
+         _activator.SetActive(true);
+          _isActivated = true;
+         //_object.SetActive(false);
+         _initialHandPosition = _activator.transform.position;
+      }
+        
 
    }
 
    private void Navigate()
    {
-      //Vector3 handOffset = _hand.position - _nintialHandPosition;
-     //Vector3 newShipPosition = _initialShipPosition + handOffset*_scaleVector;
-    //  if (handOffset.magnitude <= _maxDistance)
-     // {
-    //     _ship.position = newShipPosition;
-    //  }
-    
-     Vector3 _rotation = _hand.rotation.eulerAngles;
-     _ship.rotation = Quaternion.Euler(_rotation.x +_rotationSensitivity, _rotation.y +_rotationSensitivity, _rotation.z  +_rotationSensitivity);
-     //_rig.position = _position.position;
-     //_rig.rotation = _position.rotation;
-     //Vector3 _camRotation = _hand.rotation.eulerAngles;
-     // _camera.rotation = Quaternion.Euler(_camRotation.x +_rotationSensitivity, _camRotation.y +_rotationSensitivity, _camRotation.z +_rotationSensitivity);
-
-     // Quaternion handDelta = _hand.rotation * Quaternion.Inverse(_initialHandRotation);
-
-     //Vector3 _rigPosition = _rig.position;
-     //_ship.rotation = handDelta * _initialShipRotation;
-     //_rig.rotation = _ship.rotation;
-     //_rig.position = _rigPosition;
+     Vector3 _handOffset = _hand.position - _initialHandPosition;
+        Vector3 _newShipPosition =_initialShipPosition + _handOffset*_scaleFactor;
+        if (_handOffset.magnitude <= _maxDistance)
+        {
+            _ship.position = _newShipPosition;
+        }
+       //Vector3 _rotation = _hand.rotation.eulerAngles;
+       //_ship.rotation = Quaternion.Euler(_rotation.x+_rotationSensitivity,_rotation.y*_rotationSensitivity, _rotation.z*_rotationSensitivity);
    }
 }
